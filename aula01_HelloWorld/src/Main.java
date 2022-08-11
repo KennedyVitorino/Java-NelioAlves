88182a5e947787504b91b07f7a21b904a07f2caa
kennedy <dkennyvs@gmail.com>
2022-08-11 09:54:49 -0300

    Hello World xD

---
+++ aula01_HelloWorld/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PrintStream out = System.out;
+		
+		out.println("Hello World!");
+
+	}
+
+}

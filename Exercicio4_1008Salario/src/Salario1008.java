4674e09e593f3b3aacd15b38a3a9f68f124d2a70
kennedy <dkennyvs@gmail.com>
2022-08-11 11:20:45 -0300

    Salario

--- Exercicio4_1008Salario/src/Salario1008.java
+++ Exercicio4_1008Salario/src/Salario1008.java
@@ -1,6 +1,13 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * Fazer um programa que leia o número de um funcionário, seu número de
+ * horas trabalhadas, o valor que recebe por hora e calcula o salário
+ * desse funcionário. A seguir, mostre o número e o salário do funcionário,
+ * com duas casas decimais.
+ */
+
 public class Salario1008 {
 
 	public static void main(String[] args) {

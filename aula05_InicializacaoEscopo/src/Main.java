e5c9cf8c97364185df3a3b78731c42d95321d689
kennedy <dkennyvs@gmail.com>
2022-08-12 09:38:49 -0300

    Inicialização e escopo

---
+++ aula05_InicializacaoEscopo/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			double price = 400.00;
+
+			double discount;
+			
+			if (price > 100.0) {
+				discount = price * 0.1;
+			}
+			else {
+				discount = 0;
+			}
+
+			System.out.println(discount);
+		
+		}
+	}
+}

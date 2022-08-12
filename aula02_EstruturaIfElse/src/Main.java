8c03ccbc6881c966dbc34488df938c8f49d7f70a
kennedy <dkennyvs@gmail.com>
2022-08-12 08:28:26 -0300

    Estrutura if, else if, else

---
+++ aula02_EstruturaIfElse/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			int hora = 5;
+			
+			System.out.println("Quantas horas? ");
+			hora = sc.nextInt();
+			
+			if (hora < 12) {
+				System.out.println("Bom dia!");
+			}
+			else if (hora >= 12 && hora < 18) {
+				System.out.println("Boa tarde!");
+			}
+			else {
+				System.out.println("Boa noite!");
+			}
+
+		}
+	}
+}

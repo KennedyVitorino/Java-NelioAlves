ce66a5e3026034e20aeab84eade5c645bac58488
kennedy <dkennyvs@gmail.com>
2022-08-15 11:28:40 -0300

    Funções para strings

--- aula11_FuncoesParaStrings/src/Courses/Main.java
+++ aula11_FuncoesParaStrings/src/Courses/Main.java
@@ -1,3 +1,4 @@
+package Courses;
 public class Main {
 
 	public static void main(String[] args) {
---
+++ aula11_FuncoesParaStrings/src/Courses/MainDois.java
@@ -0,0 +1,10 @@
+package Courses;
+
+public class MainDois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

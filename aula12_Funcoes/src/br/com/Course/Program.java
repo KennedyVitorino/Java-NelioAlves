ea161ad3093c2f48b3b921683c282b194a133f3f
kennedy <dkennyvs@gmail.com>
2022-08-18 09:20:06 -0300

    Funções

---
+++ aula12_Funcoes/src/br/com/Course/Program.java
@@ -0,0 +1,29 @@
+package br.com.Course;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		try (Scanner sc = new Scanner(System.in)) {
+
+			System.out.println("Enter three numbers: ");
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+
+			if (a > b && a > c) {
+				System.out.println("Higher = " + a);
+			}
+			else if (b > c ) {
+				System.out.println("Higher = " + b);
+			}	
+			else {
+				System.out.println("Higher = " + c);
+
+			}
+
+		}	
+	}
+}

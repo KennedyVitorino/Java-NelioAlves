22868ec8e610a90a56b32650be59874729f4dce9
kennedy <dkennyvs@gmail.com>
2022-09-19 11:14:59 -0300

    Listas

--- aula22_ListasPt1/src/main/java/application/Program.java
+++ aula22_ListasPt1/src/main/java/application/Program.java
@@ -12,38 +12,59 @@ public class Program {
 
         List<String> list = new ArrayList<>();
 
-        //Adicionar elementos a lista
+        System.out.println("Lista vazia:\n" + list);
+        System.out.println("------------------------------");
+
+        // Adicionando elementos a lista
+        System.out.println("Adicionando elementos a lista:");
         list.add("Maria");
         list.add("Alex");
         list.add("Bob");
         list.add("Anna");
         list.add(2, "Marco Antonio");
-
-        // Tamanho da lista
-        System.out.println("Tamanho da lista: " + list.size());
-        System.out.println("------------------------------");
-
         for (String x : list) {
             System.out.println(x);
         }
         System.out.println("------------------------------");
 
-        /*Remover da lista
+        // Tamanho da lista
+        System.out.println("Tamanho da lista: " + list.size());
+        System.out.println("------------------------------");
+
+
+        /* Remover da lista
         list.remove(1);
         Remover da lista a partir de um PREDICADO*/
+        System.out.println("Removendo elementos da lista a partir de um PREDICADO: ");
         list.removeIf(x -> x.charAt(0) == 'M');
         for (String x : list) {
             System.out.println(x);
 
         }
 
+        //Encontrar a posição de um elemento
         System.out.println("------------------------------");
+        System.out.println("Encontrar a posição de um elemento:");
         System.out.println("Index of Bob: " + list.indexOf("Bob"));
 
-        // retorna -1 quando não encontra o elemento
+        // Retorna -1 quando não encontra o elemento
         System.out.println("Index of Marco: " + list.indexOf("Marco"));
 
+        // Filtrar elementos da lista a partir de um PREDICADO
         System.out.println("------------------------------");
+        System.out.println("Filtrar elementos da lista a partir de um PREDICADO:");
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').toList();
+        for(String x : result) {
+            System.out.println(x);
+        }
+
+        System.out.println("------------------------------");
+        String name = list.stream().filter(x -> x.charAt(0) == 'J').findFirst().orElse(null);
+        System.out.println(name);
+
+
+
+
 
     }
 }

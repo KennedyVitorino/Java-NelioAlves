bd818cbc776e3dc8480d54691f3e0ff9a901590e
kennedy <dkennyvs@gmail.com>
2022-09-19 14:44:36 -0300

    Ex - Diagonal principal e valores negativos da matriz

---
+++ Exercicio14_Matrizes/src/main/java/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+/**
+ * @author Davyd
+ *  Faça um programa para ler um ´numero inteiro N e uma matriz de ordem N
+ *  contendo números inteiros. Em seguida, mostrar a diagonal principal e
+ *  aquantidade e valores negativos da matriz
+ */
+
+public class Program {
+
+    public static void main(String[] args) {
+        // TODO comment
+    }
+}

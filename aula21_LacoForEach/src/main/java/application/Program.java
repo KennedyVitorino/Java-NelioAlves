0f89120375bbeaf68b7ff114436c5cffbd221663
kennedy <dkennyvs@gmail.com>
2022-09-19 10:02:52 -0300

    For each

---
+++ aula21_LacoForEach/src/main/java/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+/**
+ * @author Davyd
+ */
+public class Program {
+
+    public static void main(String[] args) {
+
+        String[] vect = new String[] {"Maria", "João", "Anta", "Antoin"};
+
+        for (int i = 0 ; i < vect.length; i++) {
+         System.out.println(vect[i]);
+        }
+
+        System.out.println("--------------------------------");
+        //Para cad OBJETO obj contido no VETOR vect FAÇA
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+
+    }
+}
